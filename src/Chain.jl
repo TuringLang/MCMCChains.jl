4da3073bf294d918786d0271ff5b46f54ead36b6
Martin Trapp <trapp.martin@gmail.com>
2018-08-16 21:14:32 +0100

    clean up

--- src/Chain.jl
+++ src/Chain.jl
@@ -1,21 +1,14 @@
 module Chain
 
+export Chain
 
 abstract type AbstractChains end
 
 struct Chains <: AbstractChains
 	value::Array{Float64, 3}
-  range::Range{Int}
+  range::AbstractRange{Int}
   names::Vector{AbstractString}
   chains::Vector{Int}
-end
-
-struct ModelChains <: AbstractChains
-	value::Array{Float64, 3}
-  range::Range{Int}
-  names::Vector{AbstractString}
-  chains::Vector{Int}
-  #model::Model
 end
 
 end # module
