53bba63ad1d7d433cd93d6de9fd47c9e398e1890
Rob J Goedman <goedman@mac.com>
2019-02-22 14:50:46 -0700

    Serialization tests

--- test/serialization_tests.jl
+++ test/serialization_tests.jl
@@ -2,43 +2,24 @@ using  MCMCChains, Test
 
 ProjDir = @__DIR__
 
-@testset "serialization write test" begin
+@testset "serialization read and write test" begin
   
-  m10_4s = read(joinpath(@__DIR__, "sections_m10.4s.jls"), MCMCChains.Chains)
+  m10_4s = read(joinpath(ProjDir, "sections_m10.4s.jls"), MCMCChains.Chains)
 
-  cnames =  ["lp__", "accept_stat__", "stepsize__" , "treedepth__",
-  "n_leapfrog__" , "divergent__", "energy__",
-  "a_1", "a_2", "a_3", "a_4", "a_5", "a_6", "a_7",        
-  "bp", "bpC"]        
+  write(joinpath(ProjDir, "sections_m10.4.1s.jls"), m10_4s)
   
-  pi = filter(p -> length(p) > 2 && p[end-1:end] == "__", cnames)
-  p = filter(p -> !(p in  pi), cnames)
-  p1 = ["bp", "bpC"]
-  p2 = filter(p -> !(p in p1), p)
-
-  c = Chains(m10_4s.value[:,:,:],
-    Symbol.(cnames),
-    Dict(
-      :parameters => Symbol.(p1),
-      :pooled => Symbol.(p2),
-      :internals => Symbol.(pi)
-    )
-  )
-  
-  write(joinpath(@__DIR__, "sections_m10.4.1s.jls"), c)
-end
-
-
-@testset "serialization read test" begin
+  open(joinpath(ProjDir, "sections_m10.4s.txt"), "w") do io
+    describe(io, m10_4s);
+  end
   
   c = read(joinpath(ProjDir, "sections_m10.4.1s.jls"), MCMCChains.Chains)
 
   tmpdir = tempdir()
-  open(joinpath(tmpdir, "sections_m10.4.1s.txt"), "w") do io
-    describe(io, c, section=:pooled);
+  open(joinpath(ProjDir, "sections_m10.4.1s.txt"), "w") do io
+    describe(io, c);
   end
 
-  @test open(f->read(f, String), joinpath(tmpdir, "sections_m10.4.1s.txt")) ==
+  @test open(f->read(f, String), joinpath(ProjDir, "sections_m10.4.1s.txt")) ==
       open(f->read(f, String), joinpath(ProjDir, "sections_m10.4s.txt"))
     
 end
