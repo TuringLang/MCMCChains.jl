6d01398953eb43f899005609008248fc74779871
Cameron Pfiffer <cpfiffer@gmail.com>
2019-03-06 05:48:24 -0800

    Updated serialization tests to use Turing.

--- test/serialization_tests.jl
+++ test/serialization_tests.jl
@@ -1,10 +1,19 @@
-using  MCMCChains, Test
+using Turing, Test
 
 ProjDir = mktempdir()
 
 @testset "serialization read and write test" begin
-    val = rand(500, 5, 1)
-    chn1 = Chains(val)
+    @model gdemo(x) = begin
+        s ~ InverseGamma(2,3)
+        m ~ Normal(0, sqrt(s))
+        for i in eachindex(x)
+            x[i] ~ Normal(m, sqrt(s))
+        end
+    end
+
+    model = gdemo([1.5, 2.0])
+    sampler = HMC(500, 0.01, 5)
+    chn1 = sample(model, sampler, save_state=true)
 
     write(joinpath(ProjDir, "chn1.jls"), chn1)
     chn2 = read(joinpath(ProjDir, "chn1.jls"), MCMCChains.Chains)
@@ -19,6 +28,10 @@ ProjDir = mktempdir()
 
     @test open(f->read(f, String), joinpath(ProjDir, "chn1.txt")) ==
         open(f->read(f, String), joinpath(ProjDir, "chn2.txt"))
+
+    # Test whether sampler state made it through serialization/deserialization.
+    chn3 = Turing.Utilities.resume(chn2, 100)
+    @test range(chn3) == 1:1:600
 end
 
 rm(ProjDir, force=true, recursive=true)
