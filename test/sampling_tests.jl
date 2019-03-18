11cba5c65eccbd88cc710cd3109b5eac550920fe
Rob J Goedman <goedman@mac.com>
2019-03-18 12:09:17 -0600

    Updated sampling_test.jl (include KernelDensity & testset brackets)

--- test/sampling_tests.jl
+++ test/sampling_tests.jl
@@ -1,19 +1,23 @@
-using Turing, MCMCChains, Test, Statistics
+using Turing, MCMCChains, KernelDensity, Test, Statistics
 import StatsBase: sample, AbstractWeights
 
-@model gdemo(x) = begin
-    m ~ Normal(1, 0.01)
-    s ~ Normal(5, 0.01)
+@testset "sampling api" begin
+
+  @model gdemo(x) = begin
+      m ~ Normal(1, 0.01)
+      s ~ Normal(5, 0.01)
+  end
+
+  model = gdemo([1.5, 2.0])
+  sampler = HMC(500, 0.01, 5)
+  chn = sample(model, sampler, save_state=true);
+
+  chn_sample = sample(chn, 5)
+  @test range(chn_sample) == 1:1:5
+
+  c = kde(reshape(convert(Array{Float64}, chn[:s].value), 500))
+  chn_weighted_sample = sample(c.x, Weights(c.density), 100000)
+
+  @test 4.9 < mean(reshape(convert(Array{Float64}, chn[:s].value), 500)) < 5.1
+
 end
-
-model = gdemo([1.5, 2.0])
-sampler = HMC(500, 0.01, 5)
-chn = sample(model, sampler, save_state=true);
-
-chn_sample = sample(chn, 5)
-@test range(chn_sample) == 1:1:5
-
-c = kde(reshape(convert(Array{Float64}, chn[:s].value), 500))
-chn_weighted_sample = sample(c.x, Weights(c.density), 100000)
-
-@test 4.9 < mean(reshape(convert(Array{Float64}, chn[:s].value), 500)) < 5.1
