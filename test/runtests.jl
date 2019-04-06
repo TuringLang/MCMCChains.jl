3647c2fcbe50d07c77fcdb4a5e83775f30dd356f
Cameron Pfiffer <cpfiffer@gmail.com>
2019-04-06 13:28:35 -0700

    Added testset to group MCMCChains tests together.

--- test/runtests.jl
+++ test/runtests.jl
@@ -10,26 +10,28 @@ to_add = [
 
 Pkg.add(to_add)
 
-# run plotting tests
-include("plot_test.jl")
+@testset "MCMCChains" begin
+    # run plotting tests
+    include("plot_test.jl")
 
-# run function tests
-include("diagnostic_tests.jl")
+    # run function tests
+    include("diagnostic_tests.jl")
 
-# run tests for missing values
-include("missing_tests.jl")
+    # run tests for missing values
+    include("missing_tests.jl")
 
-# run tests for missing values
-include("sections_tests.jl")
+    # run tests for missing values
+    include("sections_tests.jl")
 
-# run tests for missing values
-include("serialization_tests.jl")
+    # run tests for missing values
+    include("serialization_tests.jl")
 
-# run tests for sampoling api
-include("sampling_tests.jl")
+    # run tests for sampling api
+    include("sampling_tests.jl")
 
-# run tests for array constructor
-include("arrayconstructor_tests.jl")
+    # run tests for array constructor
+    include("arrayconstructor_tests.jl")
 
-# run tests for dataframe constructor
-include("dfconstructor_tests.jl")
+    # run tests for dataframe constructor
+    include("dfconstructor_tests.jl")
+end
