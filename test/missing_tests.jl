e8f64659f1b2f3cd3893354143835eb6e1afdcd8
Cameron Pfiffer <cpfiffer@gmail.com>
2019-04-13 20:56:55 -0700

    Minor shuffling around of missing_tests.jl

--- test/missing_tests.jl
+++ test/missing_tests.jl
@@ -17,29 +17,38 @@ end
       MCMCChains.quantile(chn)
 
     # Add missing values.
-    chn = Chains(cat(chn.value, ones(1, 2, 2) .* missing, dims = 1))
+    chn_m = Chains(cat(chn.value, ones(1, 2, 2) .* missing, dims = 1))
 
     # Call describe with missing values.
-    describe(devnull, chn; showall=true)
+    describe(devnull, chn_m; showall=true)
     m1, m2 = MCMCChains.summarystats(chn),
       MCMCChains.quantile(chn)
 
     @test s1[:,2:4] == m1[:,2:4]
-    @test all(s1[:,5] .+ 1 .== m1[:,5])
 end
 
 @testset "diagnostic functions" begin
-    chn = Chains(randn(5000, 2, 2))
+    nchains = 2
+    chn = Chains(randn(5000, 2, nchains))
 
     # Add missing values.
-    chn_m = Chains(cat(chn.value, ones(1, 2, 2) .* missing, dims = 1))
+    chn_m = Chains(cat(chn.value, ones(1, 2, nchains) .* missing, dims = 1))
 
     # Currently we only check if diag. functions throw an assertion if a value is missing.
     @test_throws AssertionError gelmandiag(chn_m)
 
-    @test all(gewekediag(chn).df .== gewekediag(chn_m).df)
-    @test all(heideldiag(chn).df .== heideldiag(chn_m).df)
-    @test all(rafterydiag(chn).df .== rafterydiag(chn_m).df)
+    gw_1 = gewekediag(chn)
+    gw_2 = gewekediag(chn_m)
+    hd_1 = heideldiag(chn)
+    hd_2 = heideldiag(chn_m)
+    rf_1 = rafterydiag(chn)
+    rf_2 = rafterydiag(chn_m)
+
+    @testset "diagnostics missing tests" for i in 1:nchains
+        @test all(gw_1[i].df == gw_2[i].df)
+        @test all(hd_1[i].df == hd_2[i].df)
+        @test all(rf_1[i].df == rf_2[i].df)
+    end
 
     @test_throws AssertionError discretediag(chn_m)
 end
