cba440234058f3793b5a28c65c2c6e451a933ad8
David Widmann <dev+git@devmotion.de>
2020-02-17 23:52:32 +0100

    Split test set to avoid timeout on Travis

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,39 +1,14 @@
 using Test
 
-@testset "MCMCChains" begin
-    # run plotting tests
-    include("plot_test.jl")
-
-    # run function tests
-    include("diagnostic_tests.jl")
-
-    # run tests for missing values
-    include("missing_tests.jl")
-
-    # run tests for describing sections
-    include("sections_tests.jl")
-
-    # run tests for accessing parameters
-    include("get_tests.jl")
-
-    # run tests for serialization
-    include("serialization_tests.jl")
-
-    # run tests for sampling api
-    include("sampling_tests.jl")
-
-    # run tests for array constructor
-    include("arrayconstructor_tests.jl")
-
-    # run tests for dataframe constructor
-    include("dfconstructor_tests.jl")
-
-    # run tests for dataframe summary
-    include("summarize_tests.jl")
-
-    # run tests for posterior stats
-    include("modelstats_test.jl")
-
-    # run tests for concatenation
-    include("concatenation_tests.jl")
-end
+@testset "Plotting" begin include("plot_test.jl") end
+@testset "Diagnostics" begin include("diagnostic_tests.jl") end
+@testset "Missing values" begin include("missing_tests.jl") end
+@testset "Sections" begin include("sections_tests.jl") end
+@testset "Accessing parameters" begin include("get_tests.jl") end
+@testset "Serialization" begin include("serialization_tests.jl") end
+@testset "Sampling" begin include("sampling_tests.jl") end
+@testset "Array constructor" begin include("arrayconstructor_tests.jl") end
+@testset "Dataframe constructor" begin include("dfconstructor_tests.jl") end
+@testset "Dataframe summary" begin include("summarize_tests.jl") end
+@testset "Posterior" begin include("modelstats_test.jl") end
+@testset "Concatenation" begin include("concatenation_tests.jl") end
