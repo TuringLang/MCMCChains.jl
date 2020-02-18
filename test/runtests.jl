bf6336504790572c483648f414278735f17387f8
David Widmann <dev+git@devmotion.de>
2020-02-18 23:29:21 +0100

    Run tests with timings

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,36 +1,36 @@
 using Test
 
 @testset "MCMCChains" begin
-    # run plotting tests
-    include("plot_test.jl")
-
     # run function tests
-    include("diagnostic_tests.jl")
+    @time include("diagnostic_tests.jl")
+
+    # run plotting tests
+    @time include("plot_test.jl")
 
     # run tests for missing values
-    include("missing_tests.jl")
+    @time include("missing_tests.jl")
 
     # run tests for describing sections
-    include("sections_tests.jl")
+    @time include("sections_tests.jl")
 
     # run tests for accessing parameters
-    include("get_tests.jl")
+    @time include("get_tests.jl")
 
     # run tests for serialization
-    include("serialization_tests.jl")
+    @time include("serialization_tests.jl")
 
     # run tests for sampling api
-    include("sampling_tests.jl")
+    @time include("sampling_tests.jl")
 
     # run tests for array constructor
-    include("arrayconstructor_tests.jl")
+    @time include("arrayconstructor_tests.jl")
 
     # run tests for dataframe constructor
-    include("dfconstructor_tests.jl")
+    @time include("dfconstructor_tests.jl")
 
     # run tests for dataframe summary
-    include("summarize_tests.jl")
+    @time include("summarize_tests.jl")
 
     # run tests for posterior stats
-    include("modelstats_test.jl")
+    @time include("modelstats_test.jl")
 end
