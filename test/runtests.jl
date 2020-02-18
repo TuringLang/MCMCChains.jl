37eebe97b2251db44481a0e8a8a8ad717eb9ce82
David Widmann <dev+git@devmotion.de>
2020-02-18 09:02:20 +0100

    Print timings of tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,14 +1,14 @@
 using Test
 
-@testset "Diagnostics" begin include("diagnostic_tests.jl") end
-@testset "Plotting" begin include("plot_test.jl") end
-@testset "Missing values" begin include("missing_tests.jl") end
-@testset "Sections" begin include("sections_tests.jl") end
-@testset "Accessing parameters" begin include("get_tests.jl") end
-@testset "Serialization" begin include("serialization_tests.jl") end
-@testset "Sampling" begin include("sampling_tests.jl") end
-@testset "Array constructor" begin include("arrayconstructor_tests.jl") end
-@testset "Dataframe constructor" begin include("dfconstructor_tests.jl") end
-@testset "Dataframe summary" begin include("summarize_tests.jl") end
-@testset "Posterior" begin include("modelstats_test.jl") end
-@testset "Concatenation" begin include("concatenation_tests.jl") end
+@time @testset "Diagnostics" begin include("diagnostic_tests.jl") end
+@time @testset "Plotting" begin include("plot_test.jl") end
+@time @testset "Missing values" begin include("missing_tests.jl") end
+@time @testset "Sections" begin include("sections_tests.jl") end
+@time @testset "Accessing parameters" begin include("get_tests.jl") end
+@time @testset "Serialization" begin include("serialization_tests.jl") end
+@time @testset "Sampling" begin include("sampling_tests.jl") end
+@time @testset "Array constructor" begin include("arrayconstructor_tests.jl") end
+@time @testset "Dataframe constructor" begin include("dfconstructor_tests.jl") end
+@time @testset "Dataframe summary" begin include("summarize_tests.jl") end
+@time @testset "Posterior" begin include("modelstats_test.jl") end
+@time @testset "Concatenation" begin include("concatenation_tests.jl") end
