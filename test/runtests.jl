67061783252c7f5b0d3c2e82fdfd6fe25b79a221
David Widmann <dev+git@devmotion.de>
2020-02-23 21:10:21 +0100

    Revert test setup

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,14 +1,51 @@
 using Test
 
-@time @testset "Diagnostics" begin include("diagnostic_tests.jl") end
-@time @testset "Plotting" begin include("plot_test.jl") end
-@time @testset "Missing values" begin include("missing_tests.jl") end
-@time @testset "Sections" begin include("sections_tests.jl") end
-@time @testset "Accessing parameters" begin include("get_tests.jl") end
-@time @testset "Serialization" begin include("serialization_tests.jl") end
-@time @testset "Sampling" begin include("sampling_tests.jl") end
-@time @testset "Array constructor" begin include("arrayconstructor_tests.jl") end
-@time @testset "Dataframe constructor" begin include("dfconstructor_tests.jl") end
-@time @testset "Dataframe summary" begin include("summarize_tests.jl") end
-@time @testset "Posterior" begin include("modelstats_test.jl") end
-@time @testset "Concatenation" begin include("concatenation_tests.jl") end
+@testset "MCMCChains" begin
+    # run plotting tests
+    println("Plotting")
+    @time include("plot_test.jl")
+
+    # run function tests
+    println("Diagnostics")
+    @time include("diagnostic_tests.jl")
+
+    # run tests for missing values
+    println("Missing values")
+    @time include("missing_tests.jl")
+
+    # run tests for describing sections
+    println("Sections")
+    @time include("sections_tests.jl")
+
+    # run tests for accessing parameters
+    println("Accessing parameters")
+    @time include("get_tests.jl")
+
+    # run tests for serialization
+    println("Serialization")
+    @time include("serialization_tests.jl")
+
+    # run tests for sampling api
+    println("Sampling")
+    @time include("sampling_tests.jl")
+
+    # run tests for array constructor
+    println("Array")
+    @time include("arrayconstructor_tests.jl")
+
+    # run tests for dataframe constructor
+    println("Dataframe")
+    @time include("dfconstructor_tests.jl")
+
+    # run tests for dataframe summary
+    println("Summary")
+    @time include("summarize_tests.jl")
+
+    # run tests for posterior stats
+    println("Model statistics")
+    @time include("modelstats_test.jl")
+
+    # run tests for concatenation
+    println("Concatenation")
+    @time include("concatenation_tests.jl")
+end
