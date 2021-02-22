4c869bc508d4b36bc5e2e08acbcfea533da38397
Rik Huijzer <t.h.huijzer@rug.nl>
2021-02-22 21:00:32 +0100

    Docs (#2)
    * Add docs for diagnostics
    
    * Remove build

---
+++ docs/make.jl
@@ -0,0 +1,17 @@
+push!(LOAD_PATH,"../src/")
+
+using Documenter
+using MCMCChains
+
+name = "MCMCChains.jl"
+
+makedocs(
+    sitename = name,
+    pages = [
+        "MCMCChains" => "index.md",
+        "Diagnostics" => "diagnostics.md"
+    ],
+    format = Documenter.HTML(prettyurls = get(ENV, "CI", nothing) == "true")
+)
+
+deploydocs(repo = "github.com/TuringLang/$name.git", devbranch = "master")
