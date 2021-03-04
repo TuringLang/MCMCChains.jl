dfd9ffc64121b65e4ec25965e9a2f39e9950d588
itsdfish <itsdfish@gmail.com>
2021-03-04 04:01:03 -0500

    fix docs, remove argument description

--- src/modelstats.jl
+++ src/modelstats.jl
@@ -5,14 +5,9 @@ export dic
 """
     dic(chain::Chains, loglik::Symbol)
 
-Compute the deviance information criterion (DIC).
+Compute the deviance information criterion (DIC) from Chain object on posterior log likelihood samples specified by varible name loglik.
 
 Note: DIC assumes that the posterior distribution is approx. multivariate Gaussian and tends to select overfitted models.
-
-# Arguments
-* `chain::Chains: an MCMC `Chain` object containing posterior log likelihood values
-* `loglik::Symbol`: variable name associated with posterior log likelihood values
-
 """
 function StatsModelComparisons.dic(chain::Chains, loglik::Symbol)
     lps = Array(chain[:, loglik, :])
