e332a6362e805fe66a8545793920529e3167beeb
Seth Axen <seth.axen@gmail.com>
2021-03-15 16:22:13 -0700

    Organize tests into sections

--- test/tables_tests.jl
+++ test/tables_tests.jl
@@ -13,63 +13,77 @@ using DataFrames
 
         @testset "Tables interface" begin
             @test Tables.istable(typeof(chn))
-            @test Tables.columnaccess(typeof(chn))
-            @test Tables.columns(chn) === chn
-            @test Tables.columnnames(chn) ==
-                  (:iteration, :chain, :a, :b, :c, :d, :e, :f, :g, :h)
-            @test Tables.getcolumn(chn, :iteration) == [1:1000; 1:1000; 1:1000; 1:1000]
-            @test Tables.getcolumn(chn, :chain) ==
-                  [fill(1, 1000); fill(2, 1000); fill(3, 1000); fill(4, 1000)]
-            @test Tables.getcolumn(chn, :a) == [
-                vec(chn[:, :a, 1])
-                vec(chn[:, :a, 2])
-                vec(chn[:, :a, 3])
-                vec(chn[:, :a, 4])
-            ]
-            @test_throws Exception Tables.getcolumn(chn, :j)
-            @test Tables.getcolumn(chn, 1) == Tables.getcolumn(chn, :iteration)
-            @test Tables.getcolumn(chn, 2) == Tables.getcolumn(chn, :chain)
-            @test Tables.getcolumn(chn, 3) == Tables.getcolumn(chn, :a)
-            @test_throws Exception Tables.getcolumn(chn, :i)
-            @test_throws Exception Tables.getcolumn(chn, 11)
-            @test Tables.rowaccess(typeof(chn))
-            @test length(Tables.rowtable(chn)) == 4000
-            nt = Tables.rowtable(chn)[1]
-            @test nt ==
-                  (; (k => Tables.getcolumn(chn, k)[1] for k in Tables.columnnames(chn))...)
-            @test nt == collect(Iterators.take(Tables.namedtupleiterator(chn), 1))[1]
-            nt = Tables.rowtable(chn)[2]
-            @test nt ==
-                  (; (k => Tables.getcolumn(chn, k)[2] for k in Tables.columnnames(chn))...)
-            @test nt == collect(Iterators.take(Tables.namedtupleiterator(chn), 2))[2]
-            @test Tables.schema(chn) isa Tables.Schema
-            @test Tables.schema(chn).names ===
-                  (:iteration, :chain, :a, :b, :c, :d, :e, :f, :g, :h)
-            @test Tables.schema(chn).types === (
-                Int,
-                Int,
-                Float64,
-                Float64,
-                Float64,
-                Float64,
-                Float64,
-                Float64,
-                Float64,
-                Float64,
-            )
-            @test Tables.matrix(chn[:, :, 1])[:, 3:end] ≈ chn[:, :, 1].value
-            @test Tables.matrix(chn[:, :, 2])[:, 3:end] ≈ chn[:, :, 2].value
-            @test Tables.matrix(Tables.rowtable(chn)) == Tables.matrix(Tables.columntable(chn))
 
-            val = rand(1000, 2, 4)
-            chn2 = Chains(val, ["iteration", "a"])
-            @test_throws Exception Tables.columns(chn2)
-            @test_throws Exception Tables.rows(chn2)
-            @test_throws Exception Tables.schema(chn2)
-            chn3 = Chains(val, ["chain", "a"])
-            @test_throws Exception Tables.columns(chn3)
-            @test_throws Exception Tables.rows(chn3)
-            @test_throws Exception Tables.schema(chn3)
+            @testset "column access" begin
+                @test Tables.columnaccess(typeof(chn))
+                @test Tables.columns(chn) === chn
+                @test Tables.columnnames(chn) ==
+                    (:iteration, :chain, :a, :b, :c, :d, :e, :f, :g, :h)
+                @test Tables.getcolumn(chn, :iteration) == [1:1000; 1:1000; 1:1000; 1:1000]
+                @test Tables.getcolumn(chn, :chain) ==
+                    [fill(1, 1000); fill(2, 1000); fill(3, 1000); fill(4, 1000)]
+                @test Tables.getcolumn(chn, :a) == [
+                    vec(chn[:, :a, 1])
+                    vec(chn[:, :a, 2])
+                    vec(chn[:, :a, 3])
+                    vec(chn[:, :a, 4])
+                ]
+                @test_throws Exception Tables.getcolumn(chn, :j)
+                @test Tables.getcolumn(chn, 1) == Tables.getcolumn(chn, :iteration)
+                @test Tables.getcolumn(chn, 2) == Tables.getcolumn(chn, :chain)
+                @test Tables.getcolumn(chn, 3) == Tables.getcolumn(chn, :a)
+                @test_throws Exception Tables.getcolumn(chn, :i)
+                @test_throws Exception Tables.getcolumn(chn, 11)
+            end
+
+            @testset "row access" begin
+                @test Tables.rowaccess(typeof(chn))
+            end
+
+            @testset "integration tests" begin
+                @test length(Tables.rowtable(chn)) == 4000
+                nt = Tables.rowtable(chn)[1]
+                @test nt ==
+                    (; (k => Tables.getcolumn(chn, k)[1] for k in Tables.columnnames(chn))...)
+                @test nt == collect(Iterators.take(Tables.namedtupleiterator(chn), 1))[1]
+                nt = Tables.rowtable(chn)[2]
+                @test nt ==
+                    (; (k => Tables.getcolumn(chn, k)[2] for k in Tables.columnnames(chn))...)
+                @test nt == collect(Iterators.take(Tables.namedtupleiterator(chn), 2))[2]
+                @test Tables.matrix(chn[:, :, 1])[:, 3:end] ≈ chn[:, :, 1].value
+                @test Tables.matrix(chn[:, :, 2])[:, 3:end] ≈ chn[:, :, 2].value
+                @test Tables.matrix(Tables.rowtable(chn)) == Tables.matrix(Tables.columntable(chn))
+            end
+
+            @testset "schema" begin
+                @test Tables.schema(chn) isa Tables.Schema
+                @test Tables.schema(chn).names ===
+                    (:iteration, :chain, :a, :b, :c, :d, :e, :f, :g, :h)
+                @test Tables.schema(chn).types === (
+                    Int,
+                    Int,
+                    Float64,
+                    Float64,
+                    Float64,
+                    Float64,
+                    Float64,
+                    Float64,
+                    Float64,
+                    Float64,
+                )
+            end
+
+            @testset "exceptions raised if reserved colname used" begin
+                val2 = rand(1000, 2, 4)
+                chn2 = Chains(val2, ["iteration", "a"])
+                @test_throws Exception Tables.columns(chn2)
+                @test_throws Exception Tables.rows(chn2)
+                @test_throws Exception Tables.schema(chn2)
+                chn3 = Chains(val2, ["chain", "a"])
+                @test_throws Exception Tables.columns(chn3)
+                @test_throws Exception Tables.rows(chn3)
+                @test_throws Exception Tables.schema(chn3)
+            end
         end
 
         @testset "TableTraits interface" begin
@@ -82,10 +96,10 @@ using DataFrames
             @test nt ==
                   (; (k => Tables.getcolumn(chn, k)[2] for k in Tables.columnnames(chn))...)
 
-            val = rand(1000, 2, 4)
-            chn2 = Chains(val, ["iteration", "a"])
+            val2 = rand(1000, 2, 4)
+            chn2 = Chains(val2, ["iteration", "a"])
             @test_throws Exception IteratorInterfaceExtensions.getiterator(chn2)
-            chn3 = Chains(val, ["chain", "a"])
+            chn3 = Chains(val2, ["chain", "a"])
             @test_throws Exception IteratorInterfaceExtensions.getiterator(chn3)
         end
 
@@ -106,29 +120,41 @@ using DataFrames
 
         @testset "Tables interface" begin
             @test Tables.istable(typeof(cdf))
-            @test Tables.columnaccess(typeof(cdf))
-            @test Tables.columns(cdf) === cdf
-            @test Tables.columnnames(cdf) == keys(cdf.nt)
-            for (k, v) in pairs(cdf.nt)
-                @test Tables.getcolumn(cdf, k) == v
+
+            @testset "column access" begin
+                @test Tables.columnaccess(typeof(cdf))
+                @test Tables.columns(cdf) === cdf
+                @test Tables.columnnames(cdf) == keys(cdf.nt)
+                for (k, v) in pairs(cdf.nt)
+                    @test Tables.getcolumn(cdf, k) == v
+                end
+                @test Tables.getcolumn(cdf, 1) == Tables.getcolumn(cdf, keys(cdf.nt)[1])
+                @test Tables.getcolumn(cdf, 2) == Tables.getcolumn(cdf, keys(cdf.nt)[2])
+                @test_throws Exception Tables.getcolumn(cdf, :blah)
+                @test_throws Exception Tables.getcolumn(cdf, length(cdf.nt) + 1)
             end
-            @test Tables.getcolumn(cdf, 1) == Tables.getcolumn(cdf, keys(cdf.nt)[1])
-            @test Tables.getcolumn(cdf, 2) == Tables.getcolumn(cdf, keys(cdf.nt)[2])
-            @test_throws Exception Tables.getcolumn(cdf, :blah)
-            @test_throws Exception Tables.getcolumn(cdf, length(cdf.nt) + 1)
-            @test Tables.rowaccess(typeof(cdf))
-            @test length(Tables.rowtable(cdf)) == length(cdf.nt[1])
-            @test Tables.columntable(cdf) == cdf.nt
-            nt = Tables.rowtable(cdf)[1]
-            @test nt == (; (k => v[1] for (k, v) in pairs(cdf.nt))...)
-            @test nt == collect(Iterators.take(Tables.namedtupleiterator(cdf), 1))[1]
-            nt = Tables.rowtable(cdf)[2]
-            @test nt == (; (k => v[2] for (k, v) in pairs(cdf.nt))...)
-            @test nt == collect(Iterators.take(Tables.namedtupleiterator(cdf), 2))[2]
-            @test Tables.schema(cdf) isa Tables.Schema
-            @test Tables.schema(cdf).names === keys(cdf.nt)
-            @test Tables.schema(cdf).types === eltype.(values(cdf.nt))
-            @test Tables.matrix(Tables.rowtable(cdf)) == Tables.matrix(Tables.columntable(cdf))
+
+            @testset "row access" begin
+                @test Tables.rowaccess(typeof(cdf))
+            end
+
+            @testset "integration tests" begin
+                @test length(Tables.rowtable(cdf)) == length(cdf.nt[1])
+                @test Tables.columntable(cdf) == cdf.nt
+                nt = Tables.rowtable(cdf)[1]
+                @test nt == (; (k => v[1] for (k, v) in pairs(cdf.nt))...)
+                @test nt == collect(Iterators.take(Tables.namedtupleiterator(cdf), 1))[1]
+                nt = Tables.rowtable(cdf)[2]
+                @test nt == (; (k => v[2] for (k, v) in pairs(cdf.nt))...)
+                @test nt == collect(Iterators.take(Tables.namedtupleiterator(cdf), 2))[2]
+                @test Tables.matrix(Tables.rowtable(cdf)) == Tables.matrix(Tables.columntable(cdf))
+            end
+
+            @testset "schema" begin
+                @test Tables.schema(cdf) isa Tables.Schema
+                @test Tables.schema(cdf).names === keys(cdf.nt)
+                @test Tables.schema(cdf).types === eltype.(values(cdf.nt))
+            end
         end
 
         @testset "TableTraits interface" begin
