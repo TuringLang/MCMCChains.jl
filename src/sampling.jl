ea6b14ed15b66f6aec8922b107f5b173632e2921
David Widmann <david.widmann@it.uu.se>
2022-04-29 02:22:55 +0200

    Fix sampling from multiple chains

--- src/sampling.jl
+++ src/sampling.jl
@@ -1,43 +1,58 @@
 """
-    sample(chn::Chains, [wv::AbstractWeights,] n; replace=true, kwargs...)
-    sample(rng::Random.AbstractRNG, chn::Chains, [wv::AbstractWeights,] n; kwargs...)
+    sample([rng,] chn::Chains, [wv::AbstractWeights,] n; replace=true, ordered=false)
 
-Sample `n` samples from `chn`; see also [`subset`](@ref).
-Optionally, the samples can be weighted using `wv`.
-Here, `kwargs` defaults to `replace=true` and `ordered=false`.
+Sample `n` samples from the pooled (!) chain `chn`.
+
+Based on the keyword arguments `replace` and `ordered` sampling is performed with replacement and the sample is ordered, respectively.
+Optionally, sampling probabilities are proportional to weights `wv`.
+
+!!! note
+    If `chn` contains multiple chains, they are pooled (i.e., appended) before sampling.
+    This ensures that even in this case exactly `n` samples are returned:
+    ```jldoctest
+    julia> chn = Chains(randn(11, 4, 3));
+
+    julia> size(sample(chn, 7)) == (7, 4)
+    true
 """
-sample(chn::Chains, n; kwargs...) = sample(Random.GLOBAL_RNG, chn, n; kwargs...)
-function sample(chn::Chains, wv::AbstractWeights, n)
-    return sample(Random.GLOBAL_RNG, chn, wv, n)
+function sample(chn::Chains, n::Integer; replace::Bool=true, ordered::Bool=false)
+    return sample(Random.GLOBAL_RNG, chn, n; replace=replace, ordered=ordered)
+end
+function sample(chn::Chains, wv::AbstractWeights, n::Integer; replace::Bool=true, ordered::Bool=false)
+    return sample(Random.GLOBAL_RNG, chn, wv, n; replace=replace, ordered=ordered)
 end
 
 function sample(
     rng::Random.AbstractRNG,
     chn::Chains,
-    n;
-    replace = true,
-    ordered = false
+    n::Integer;
+    replace::Bool = true,
+    ordered::Bool = false
 )
-    return subset(chn, sample(rng, range(chn), n; replace = replace, ordered = ordered))
+    return _sample(rng, chn, n; replace=replace, ordered=ordered)
 end
+
 
 function sample(
     rng::Random.AbstractRNG,
     chn::Chains,
     wv::AbstractWeights,
-    n
+    n::Integer;
+    replace::Bool = true,
+    ordered::Bool = false
 ) 
-    return subset(chn, sample(rng, range(chn), wv, n))
+    return _sample(rng, chn, wv, n; replace=replace, ordered=ordered)
 end
 
-"""
-    subset(chn::Chains, samples)
+# Internal implementation with generic arguments (possibly including weights) and keyword arguments
+# This is not exposed to avoid method ambiguities and to accept incorrect (keyword) arguments 
+function _sample(rng, chn, args...; kwargs...)
+    data = chn.value.data
+    pool_data = reshape(PermutedDimsArray(data, (1, 3, 2)), :, size(data, 2), 1)
+    idxs = sample(rng, axes(pool_data, 1), args...; kwargs...)
 
-Return a subset of samples.
-"""
-function subset(chn::Chains, samples)
-    data = AxisArray(chn.value[samples, :, :].data;
-                     iter = 1:length(samples), var = names(chn), chain = chains(chn))
+    samples = AxisArray(pool_data[idxs, :, :]; iter=1:length(idxs), var=names(chn), chain=1:1)
 
-    return Chains(data, missing, chn.name_map, chn.info)
+    return Chains(samples, missing, chn.name_map, chn.info)
 end
+
