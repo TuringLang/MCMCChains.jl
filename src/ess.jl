2acafe8f3258b73b59f88aee13539cffb9242b86
Seth Axen <seth@sethaxen.com>
2023-02-27 14:34:25 +0100

    Overload and update ess and rhat

--- src/ess.jl
+++ src/ess.jl
@@ -1,7 +1,66 @@
 """
+    ess(chains::Chains; duration=compute_duration, kwargs...)
+
+Estimate the effective sample size.
+
+ESS per second options include `duration=MCMCChains.compute_duration` (the default)
+and `duration=MCMCChains.wall_duration`.
+"""
+function MCMCDiagnosticTools.ess(
+    chains::Chains;
+    sections = _default_sections(chains), duration = compute_duration, kwargs...
+)
+    # Subset the chain
+    _chains = Chains(chains, _clean_sections(chains, sections))
+
+    # Estimate the effective sample size
+    ess = MCMCDiagnosticTools.ess(
+        _permutedims_diagnostics(_chains.value.data);
+        kwargs...,
+    )
+
+    # Calculate ESS/minute if available
+    dur = duration(chains)
+
+    # Convert to NamedTuple
+    nt = if dur === missing
+        merge((parameters = names(_chains),), (; ess))
+    else
+        ess_per_sec = ess_rhat.ess/dur
+        merge((parameters = names(_chains),), (; ess, ess_per_sec))
+    end
+
+    return ChainDataFrame("ESS", nt)
+end
+
+"""
+    rhat(chains::Chains; kwargs...)
+
+Estimate the ``\\widehat{R}`` diagnostic.
+"""
+function MCMCDiagnosticTools.rhat(
+    chains::Chains;
+    sections = _default_sections(chains), kwargs...
+)
+    # Subset the chain
+    _chains = Chains(chains, _clean_sections(chains, sections))
+
+    # Estimate the rhat
+    rhat = MCMCDiagnosticTools.rhat(
+        _permutedims_diagnostics(_chains.value.data);
+        kwargs...,
+    )
+
+    # Convert to NamedTuple
+    nt = merge((parameters = names(_chains),), (; rhat))
+
+    return ChainDataFrame("R-hat", nt)
+end
+
+"""
     ess_rhat(chains::Chains; duration=compute_duration, kwargs...)
 
-Estimate the effective sample size and the potential scale reduction.
+Estimate the effective sample size and the ``\\widehat{R}`` diagnostic
 
 ESS per second options include `duration=MCMCChains.compute_duration` (the default)
 and `duration=MCMCChains.wall_duration`.
@@ -14,7 +73,7 @@ function MCMCDiagnosticTools.ess_rhat(
     _chains = Chains(chains, _clean_sections(chains, sections))
 
     # Estimate the effective sample size and rhat
-    ess, rhat = MCMCDiagnosticTools.ess_rhat(
+    ess_rhat = MCMCDiagnosticTools.ess_rhat(
         _permutedims_diagnostics(_chains.value.data);
         kwargs...,
     )
@@ -24,10 +83,11 @@ function MCMCDiagnosticTools.ess_rhat(
 
     # Convert to NamedTuple
     nt = if dur === missing
-        merge((parameters = names(_chains),), (ess = ess, rhat = rhat))
+        merge((parameters = names(_chains),), ess_rhat)
     else
-        merge((parameters = names(_chains),), (ess = ess, rhat = rhat, ess_per_sec=ess/dur))
+        ess_per_sec = ess_rhat.ess/dur
+        merge((parameters = names(_chains),), ess_rhat, (; ess_per_sec))
     end
 
-    return ChainDataFrame("ESS", nt)
+    return ChainDataFrame("ESS/R-hat", nt)
 end
