4e99fe8f069f6908ede8f9d3bd8b2994fd22c119
Seth Axen <seth@sethaxen.com>
2023-08-20 15:57:40 +0200

    Update docstring

--- src/summarize.jl
+++ src/summarize.jl
@@ -122,9 +122,26 @@ function Base.convert(::Type{Array}, cs::Array{ChainDataFrame{T},1}) where T<:Na
 end
 
 """
-    summarize(chains, funs...[; sections, name = "", append_chains = true])
+    summarize(
+        chains[, stats_funs...];
+        append_chains=true,
+        name="SummaryStats",
+        [sections, var_names],
+    )
 
 Summarize `chains` in a `PosteriorStats.SummaryStats`.
+
+`stats_funs` is a collection of functions that reduces a matrix with shape `(draws, chains)`
+to a scalar or a collection of scalars. Alternatively, an item in `stats_funs` may be a
+`Pair` of the form `name => fun` specifying the name to be used for the statistic or of the
+form `(name1, ...) => fun` when the function returns a collection. When the function returns
+a collection, the names in this latter format must be provided.
+
+If no stats functions are provided, then those specified in [`default_summary_stats`](@ref)
+are computed.
+
+`var_names` specifies the names of the parameters in data. If not provided, the names are
+inferred from data.
 
 # Examples
 
@@ -137,13 +154,14 @@ function PosteriorStats.summarize(
     chains::Chains, funs...;
     sections = _default_sections(chains),
     append_chains::Bool = true,
+    var_names=nothing,
     kwargs...
 )
     # Generate a chain to work on.
     chn = Chains(chains, _clean_sections(chains, sections))
 
     # Obtain names of parameters.
-    names_of_params = names(chn)
+    names_of_params = var_names === nothing ? names(chn) : var_names
 
     if append_chains
         # Evaluate the functions.
