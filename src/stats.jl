e24a5eb4380fb69293a7d2e62d8804be52d475dd
Shravan Goswami <shravanngoswamii@gmail.com>
2025-05-18 20:42:00 +0530

    Formatting fix

--- src/stats.jl
+++ src/stats.jl
@@ -172,9 +172,9 @@ function changerate(chains::AbstractArray{<:Real,3})
 end
 
 """
-    describe(io, chains[; 
-             q=[0.025, 0.25, 0.5, 0.75, 0.975], 
-             etype=:bm, 
+    describe(io, chains[;
+             q = [0.025, 0.25, 0.5, 0.75, 0.975],
+             etype = :bm,
              kwargs...])
 Print chain metadata, summary statistics, and quantiles. Use `describe(chains)` for REPL output to `stdout`, or specify `io` for other streams (e.g., file output).
 """
