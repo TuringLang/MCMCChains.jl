0a3e7372e0c193329a6253128798127b2a4fb4bc
Shravan Goswami <shravanngoswamii@gmail.com>
2025-08-18 03:19:50 +0530

    run Julia Formatter

--- src/plot.jl
+++ src/plot.jl
@@ -171,7 +171,7 @@ const supportedplots = push!(collect(keys(translationdict)), :mixeddensity, :cor
     end
 
     if st == :autocorplot
-        lags = 0:(maxlag===nothing ? round(Int, 10*log10(length(range(c)))) : maxlag)
+        lags = 0:(maxlag === nothing ? round(Int, 10 * log10(length(range(c)))) : maxlag)
         # Chains are already appended in `c` if desired, hence we use `append_chains=false`
         ac = autocor(c; sections = nothing, lags = lags, append_chains = false)
         ac_mat = convert(Array{Float64}, ac)
