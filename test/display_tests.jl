b5da89f9997223f44896ce4f7a06283641adeef2
Penelope Yong <penelopeysm@gmail.com>
2025-10-26 12:00:10 +0000

    Add a test

--- test/display_tests.jl
+++ test/display_tests.jl
@@ -1,3 +1,6 @@
-@testset "describe" begin
-    @test describe(Chains(rand(10, 3, 1))) isa Any
+@testset "describe / show" begin
+    c = Chains(rand(10, 3, 2))
+    @test describe(c) isa Any
+    @test show(c) isa Any
+    @test show(mean(c)) isa Any
 end
